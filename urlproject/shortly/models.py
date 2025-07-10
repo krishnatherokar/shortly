4c1db849968831519f3b8e827afbd9f61f913a65
Krishna Therokar <krishnatherokar@gmail.com>
2025-07-10 19:26:11 +0530

    feat: add ShortUrl model, register it and make migrations

--- urlproject/shortly/models.py
+++ urlproject/shortly/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class ShortUrl(models.Model):
+    original_url = models.URLField()
+    short_code = models.CharField(max_length=10, unique=True)
